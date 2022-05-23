d45d9a722541376e716def6147c8adbecec9d312
Luke D'Amato <luke.damato.20@um.edu.mt>
2022-05-23 11:02:09 +0200

    added init fixed dir

---
+++ Assignment/dfa.py
@@ -0,0 +1,36 @@
+# DSA2 Assignment
+# Graph Algorithms on DFAs
+
+import random
+
+class DFA:
+
+    # region Question 1
+    # initialising an init function for the creating of the DFA
+    def __init__(self, states, accepting, rejecting, symbol, starting, transition):
+        self.states = states
+        self.accepting = accepting
+        self.rejecting = rejecting
+        self.symbol = symbol
+        self.starting = starting
+        self.transition = transition
+    # endregion
+
+    # region Question 2
+
+    # endregion
+
+    # region Question 3
+
+    # endregion
+
+    # region Question 4
+
+    # endregion
+
+    # region Question 5
+
+    # endregion
+
+
+
